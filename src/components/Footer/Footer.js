ccda3b42c85c4b6d5deec254900b13750c3a3734
Isaac <82594024+izaqueIsrael@users.noreply.github.com>
2023-03-19 15:33:45 -0300

    alterações do card do trello

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,7 @@ function Footer() {
   return (
     <div className="footer">
       <p>
-        Desenvolvido por: Diego, Gabriel, Haina, Izaque, João Fernando, Matheus,
-        Maycon, Nuno.
+        Desenvolvido por: <a href='https://github.com/diego-analytics'>Diego</a>, <a href='https://github.com/Aguiar-Gabriel'>Gabriel</a>, <a href='https://github.com/hainaha'>Haina</a>, <a href='https://github.com/izaqueIsrael'>Izaque</a>, <a href='https://github.com/joaofq'>João Fernando</a>, <a href='https://github.com/evaristomat'>Matheus</a>, <a href='https://github.com/NunoMac'>Nuno</a>.
       </p>
       <p>1º Hackatoon da Practicum - Brasil</p>
     </div>
